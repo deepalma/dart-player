a9cc1bb102d7661b0908f167af5101d86b39805e
Francesco Di Palma <francescodip@gmail.com>
2017-01-22 14:20:52 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Dart player!</h2>
+        </div>
+        <p className="App-intro">
+          To get started, load a flac file.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
